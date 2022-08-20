0f58107637dab7adcd3eb7f9e1aec216b8e66089
Nathan da Silva Lira <108292134+bynathan@users.noreply.github.com>
2022-08-20 06:56:19 -0300

    Update script.js

--- resources/global/js/script.js
+++ resources/global/js/script.js
@@ -1,3 +1,7 @@
+window.confirm('Projeto, em Desenvolvimento.')
+window.confirm('Clique em OK, para vizualizar o Projeto.')
+window.confirm('Este é o link para visualizar o diretorio do Projeto no GitHub:  https://bynathan.github.io/project-figmaland-002/')
+
 const inptNavbarEmail = document.getElementById('inpt-navbar-email');
 const buttonNavbarSubscribe = document.getElementById('button-navbar-subscribe');
 
